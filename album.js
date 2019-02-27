b8c96d92a771e64cefcf8cc579a2d04dbbb063d2
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-02-26 20:32:09 -0500

    Update album.js

--- album.js
+++ album.js
@@ -3,3 +3,16 @@ const mysql = require('./../dbcon.js');
 const moment = require('moment');
 const router = express.Router();
 
+//returns table in JSON format 
+router.get('/', (req, res, next) =>
+{
+    mysql.pool.query('SELECT album_name AS name FROM album', (err, rows, fields) => 
+	  {
+	      if (err)
+        return next(err);
+        res.json(rows)
+    });
+});
+
+
+
