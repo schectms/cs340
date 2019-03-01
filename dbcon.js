f7362b0a71a0a160f9d8686959e395bd29ce3ae0
Christian Nelson <christiannelson@christians-mbp.lan>
2019-02-28 20:17:19 -0500

    Removed password

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_nelsonc4',
-  password        : '2642',
+  password        : '----',
   database        : 'cs340_nelsonc4'
 });
 module.exports.pool = pool;
