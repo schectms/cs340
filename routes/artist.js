987a5d46311bc16f436b75f2c8218e7b899ccf18
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-03-04 20:12:13 -0500

    Update artist.js

--- routes/artist.js
+++ routes/artist.js
@@ -1,8 +1,8 @@
-module.exports = function(){
+	module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    function getArtists(req, res, mysql, context, complete){
+    function getArtists(res, mysql, context, complete){
         mysql.pool.query("SELECT artist.artist_name, artist.artist_genre, artist.artist_bio, artist.artist_id FROM artist", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
@@ -14,9 +14,9 @@ module.exports = function(){
     }
 
      
-    function getArtist(req, res, mysql, context, complete){
+    function getArtist( res, mysql, context, complete){
         var sql = "SELECT artist.artist_name, artist.artist_genre, artist.artist_bio, artist.artist_id AS id FROM artist WHERE artist_id = ?";
-        var inserts = ["3"]; // this needs to be the artist id from the req
+        var inserts = [id]; // this needs to be the artist id from the req
         mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
@@ -47,7 +47,7 @@ module.exports = function(){
         var callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
-        getArtists(req, res, mysql, context, complete);
+        getArtists( res, mysql, context, complete);
         function complete(){
             res.render('artists', context);
         }
@@ -58,7 +58,7 @@ module.exports = function(){
     router.post('/', function(req, res){
         var mysql = req.app.get('mysql');
         var sql = "INSERT INTO artist (artist_name, artist_genre, artist_bio) VALUES (?, ?, ?)";
-        var inserts = [req.body.artist.name, req.body.artist.genre, req.body.artist.bio];
+        var inserts = [req.body.artist_name, req.body_artist_genre, req.body.artist_bio];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
@@ -72,3 +72,4 @@ module.exports = function(){
 
     return router;
 }();
+
