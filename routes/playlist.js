6336e57d14821a78f4c2ba8a305befddf7a3e2bb
Christian Nelson <christiannelson@10-197-34-55.sds.oregonstate.edu>
2019-03-05 22:42:26 -0500

    playlist functional

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,59 +1,79 @@
-module.exports = function(){
-    var express = require('express');
-    var router = express.Router();
+module.exports = (function() {
+	var express = require('express');
+	var router = express.Router();
 
-    function getPlaylists(req, res, mysql, context, complete){
-        mysql.pool.query("SELECT playlist.playlist_name, user.user_name, playlist.playlist_id FROM playlist INNER JOIN user ON user.user_id = playlist.uid", function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.playlists  = results;
-            complete();
-        });
-    }
+	function getPlaylists(req, res, mysql, context, complete) {
+		mysql.pool.query(
+			'SELECT playlist.playlist_name, user.user_name, playlist.playlist_id FROM playlist INNER JOIN user ON user.user_id = playlist.uid',
+			function(error, results, fields) {
+				if (error) {
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				context.playlists = results;
+				complete();
+			}
+		);
+	}
 
-    function getUsersPlaylists(req, res, mysql, context, complete){
-        var sql = "SELECT playlist.playlist_name, user.user_name, user.user_id FROM playlist INNER JOIN user ON user.user_id = playlist.uid WHERE user.user_id = ?";
-        var inserts = ["3"]; // this needs to be the user id from the req
-        mysql.pool.query(sql, inserts, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.playlists = results;
-            complete();
-        });
-    }
-    
-    /* READ - Display all playlists*/
+	function getUsersPlaylists(req, res, mysql, context, complete) {
+		var sql =
+			'SELECT playlist.playlist_name, user.user_name, user.user_id FROM playlist INNER JOIN user ON user.user_id = playlist.uid WHERE user.user_id = ?';
+		var inserts = [ '3' ]; // this needs to be the user id from the req
+		mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if (error) {
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			context.playlists = results;
+			complete();
+		});
+	}
 
-    router.get('/', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        var mysql = req.app.get('mysql');
-        getPlaylists(req, res, mysql, context, complete);
-        function complete(){
-            res.render('playlists', context);
-        }
-    });
+	function getUsersForDropDown(req, res, mysql, context, complete) {
+		var sql = 'SELECT user.user_name, user.user_id FROM user';
+		mysql.pool.query(sql, function(error, results, fields) {
+			if (error) {
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			context.users_dropdown = results;
+			complete();
+		});
+	}
 
-    /* CREATE - Adds a playlist */
+	/* READ - Display all playlists*/
 
-    router.post('/', function(req, res){
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO playlist (playlist_name, uid) VALUES(?, ?)";
-        var inserts = [req.body.playlist.name, req.body.playlist.user_id];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/playlists');
-            }
-        });
-    });
+	router.get('/', function(req, res) {
+		var callbackCount = 0;
+		var context = {};
+		var mysql = req.app.get('mysql');
+		getPlaylists(req, res, mysql, context, complete);
+		getUsersForDropDown(req, res, mysql, context, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 2) {
+				res.render('playlists', context);
+			}
+		}
+	});
 
-    return router;
-}();
+	/* CREATE - Adds a playlist */
+
+	router.post('/', function(req, res) {
+		var mysql = req.app.get('mysql');
+		var sql = 'INSERT INTO playlist (playlist_name, uid) VALUES(?, ?)';
+		var inserts = [ req.body.playlist_name, req.body.user_created ];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if (error) {
+				console.log(JSON.stringify(error));
+				res.write(JSON.stringify(error));
+				res.end();
+			} else {
+				res.redirect('/playlists');
+			}
+		});
+	});
+
+	return router;
+})();
