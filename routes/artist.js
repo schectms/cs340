a044206a3cd50d6a2903d53945c8873ce6a77f8d
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-03-07 17:58:30 -0500

    Update artist.js

--- routes/artist.js
+++ routes/artist.js
@@ -87,9 +87,9 @@
 	router.put('/:artist_id', function(req, res){
         var mysql = req.app.get('mysql');
 //        console.log(req.body)
-        console.log(req.params.artist_id);
+        console.log(req.query.artist_id);
         var sql="UPDATE artist SET artist_name = ?, artist_genre = ?, artist_bio = ? WHERE artist_id=21";
-	var inserts = [req.body.artist_name, req.body.artist_genre, req.body.artist_bio, req.params.artist_id];
+	var inserts = [req.body.artist_name, req.body.artist_genre, req.body.artist_bio, req.query.artist_id];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(error)
