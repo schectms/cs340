897068392e4aba32e0192bc3a58d012c0ffd9f8b
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-03-07 10:35:05 -0500

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -75,6 +75,21 @@ module.exports = (function() {
 			}
 		});
 	});
+	
+	router.delete('/:playlist_id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM playlist WHERE playlist_id = ?";
+        var inserts = [req.params.playlist_id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
 
 	return router;
 })();
