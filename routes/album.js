19716fbee22f3963973858df010ac84482381bcd
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-03-07 10:34:04 -0500

    Update album.js

--- routes/album.js
+++ routes/album.js
@@ -62,6 +62,21 @@ module.exports = (function() {
 			}
 		});
 	});
+	
+	 router.delete('/:album_id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM album WHERE album_id = ?";
+        var inserts = [req.params.album_id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
 
 	return router;
 })();
