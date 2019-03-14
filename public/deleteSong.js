c3ce4b651fc0589d192a5b452167ba2d570231e1
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-03-13 22:34:10 -0400

    Update deleteSong.js

--- public/deleteSong.js
+++ public/deleteSong.js
@@ -1,6 +1,6 @@
 function deleteSong(song_id){
     $.ajax({
-        url: '/artists/' + song_id,
+        url: '/songs/' + song_id,
         type: 'DELETE',
         success: function(result){
             window.location.reload(true);
